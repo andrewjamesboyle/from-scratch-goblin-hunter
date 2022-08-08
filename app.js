ef17e644cc0ebb1040c1d44d21317e4f6b3c73d3
ANDREW JAMES BOYLE <ANDREWBOYLEMUSIC@GMAIL.COM>
2022-08-08 09:59:07 -0700

    goblin.hp > 0 conditional

--- app.js
+++ app.js
@@ -63,27 +63,29 @@ function displayGoblins() {
 
     for (let goblin of goblinsArray) {
         const goblinEl = renderGoblin(goblin);
+        if (goblin.hp > 0) {
 
-        if (goblin.hp > 0 && playerHp > 0) {
-            goblinEl.addEventListener('click', () => {
-                if (Math.random() > 0.5) {
-                    goblin.hp--;
-                    alert(`Smack! ${goblin.name} oughta feel that one in the morning!`);
-                } else {
-                    alert(`You missed ${goblin.name}!`);
-                }
-                if (Math.random() > 0.7) {
-                    playerHp--;
-                    alert(`You got smacked by ${goblin.name}!`);
-                }
-                if (goblin.hp === 0) {
-                    defeatedGoblins++;
-                    alert(`You killed ${goblin.name}!!! Bye Felicia!`);
-                }
-                playerHpEl.textContent = `Your HP is ${playerHp}`;
-                displayGoblins();
-            });
-        } 
+            if (goblin.hp > 0 && playerHp > 0) {
+                goblinEl.addEventListener('click', () => {
+                    if (Math.random() > 0.5) {
+                        goblin.hp--;
+                        alert(`Smack! ${goblin.name} oughta feel that one in the morning!`);
+                    } else {
+                        alert(`You missed ${goblin.name}!`);
+                    }
+                    if (Math.random() > 0.7) {
+                        playerHp--;
+                        alert(`You got smacked by ${goblin.name}!`);
+                    }
+                    if (goblin.hp === 0) {
+                        defeatedGoblins++;
+                        alert(`You killed ${goblin.name}!!! Bye Felicia!`);
+                    }
+                    playerHpEl.textContent = `Your HP is ${playerHp}`;
+                    displayGoblins();
+                });
+            } 
+        }
         goblinContainerEl.append(goblinEl);
     }
     
